79dfcdc5ac2ca793189dabc55dc665cf1cff1fdc
Cody Fisher <457r0ch1mp@gmail.com>
2019-09-21 22:54:13 -0400

    Committed on new branch

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,26 @@
 from flask import Flask
 from config import Config
+<<<<<<< Updated upstream
+||||||| merged common ancestors
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+=======
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+>>>>>>> Stashed changes
 
 app = Flask(__name__)
 app.config.from_object(Config)
+<<<<<<< Updated upstream
+||||||| merged common ancestors
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+=======
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+>>>>>>> Stashed changes
 
 from app import routes
