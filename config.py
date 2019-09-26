d4bab9d75035c03d4c404fbeb653d8b5411f3b17
Cody Fisher <457r0ch1mp@gmail.com>
2019-09-26 13:15:18 -0400

    Spelling error

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['microblog.help@gmail.com']
-    POST_PER_PAGE = 3
+    POSTS_PER_PAGE = 3
