9a1ee2cf08d49bc786801beb54bcb67c02ad697d
Cody Fisher <457r0ch1mp@gmail.com>
2019-10-03 18:51:18 -0400

    Add AWS config vars
    Authenticating with AWS for Translate API

--- config.py
+++ config.py
@@ -15,3 +15,6 @@ class Config(object):
     ADMINS = ['microblog.help@gmail.com']
     POSTS_PER_PAGE = 25
     LANGUAGES = ['en', 'es']
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_DEFAULT_REGION = os.environ.get('AWS_DEFAULT_REGION') or 'us-east-1'
